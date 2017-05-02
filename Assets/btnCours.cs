6cf8e783e4fab37ec7a36b9db5d629e41f1485b0
Kevin Mandresy VELIA <kmvelia@MacBook-Pro-de-Kevin.local>
2017-05-03 00:30:05 +0200

    Add 'scene_cours' + videoPlayer object on it [en Cours de dev]

---
+++ Assets/btnCours.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class btnCours : MonoBehaviour
+{
+
+	public Button buttonCours;
+	// Use this for initialization
+	void Start() {
+		Button btnCours = buttonCours.GetComponent<Button>();
+		btnCours.onClick.AddListener(GoToCours);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+	void GoToCours() {
+		SceneManager.LoadScene("scene_cours");
+	}
+}
