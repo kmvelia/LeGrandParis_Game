480b33843f16e59ec51a17f81e447a16cab5e127
Kevin Mandresy VELIA <kmvelia@MacBook-Pro-de-Kevin.local>
2017-05-03 20:37:46 +0200

    Aucune modification majeur

--- Assets/script/game1lvldebutant.cs
+++ Assets/script/game1lvldebutant.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class game1lvldebutant : MonoBehaviour {
 
