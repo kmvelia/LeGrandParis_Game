3b1e91db1f35611a877a4719e483b2fa62be5e46
Alias <wahbi_i@etna-alternance.net>
2017-05-04 20:27:22 +0200

    Commencement jeux 2
    Commencement jeux 2

---
+++ Assets/_mysql.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _mysql : MonoBehaviour
+{
+    string highscore_url = "http://51.254.114.220/get_math_enonce.php";
+    string playName = "Player 1";
+    int score = -1;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        // Create a form object for sending high score data to the server
+        WWWForm form = new WWWForm();
+        // Assuming the perl script manages high scores for different games
+        form.AddField("action", "get_enonce");
+        // The name of the player submitting the scores
+        //form.AddField("playerName", playName);
+        // The score
+        //form.AddField("score", score);
+
+        // Create a download object
+        WWW download = new WWW(highscore_url, form);
+
+        // Wait until the download is done
+        yield return download;
+
+        if (!string.IsNullOrEmpty(download.error))
+        {
+            print("Error downloading: " + download.error);
+        }
+        else
+        {
+            // show the highscores
+            Debug.Log(download.text);
+        }
+    }
+}
