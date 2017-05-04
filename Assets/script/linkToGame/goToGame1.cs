77960c1339057ec8e84927bdf7f5e25b749be437
VeliaKevinMandresy <kmvelia@MacBook-Pro-de-Kevin.local>
2017-05-04 17:24:53 +0200

    Add all scenes_cours

--- Assets/script/linkToGame/goToGame1.cs
+++ Assets/script/linkToGame/goToGame1.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 
 public class goToGame1 : MonoBehaviour {
 
+	public Button btnGoToGame2_cours;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+		Button btnGame2_cours = GetComponent<Button>();
+		btnGoToGame2_cours.onClick.AddListener(LoadGame2_cours.);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+
+	}
+
+	void LoadGame2_cours(int test) {
+		SceneManager.LoadScene("Game2_cours");	
 	}
 }
