e2bb6b5645be55e8b1762d2e73c1e823b0752204
VeliaKevinMandresy <kmvelia@MacBook-Pro-de-Kevin.local>
2017-05-04 22:53:52 +0200

    [Suppression + modification] Suppression/modification scripts

---
+++ Assets/script/switch_scene.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class switch_scene : MonoBehaviour {
+
+	public Button actionButton;
+	public int levelIndex;
+
+	// Use this for initialization
+	void Start()
+	{
+		Button btn = actionButton.GetComponent<Button>();
+		btn.onClick.AddListener(LoaderScene);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	// Load Scene stock in Arrays of String 
+	void LoaderScene()
+	{ 
+		string[] sceneName = { "scene_accueil", // 0
+			"scene_cours", // 1
+			"scene_game1chooselevel", // 2
+			"scene_game1debutant", // 3
+			"scene_game1debutant_region", // 4
+			"Game1_cours", // 5
+			"Game2_cours", // 6
+			"Game3_cours", // 7
+			"Game4_cours", // 8
+			"Game5_cours" }; // 9
+		SceneManager.LoadScene(sceneName[levelIndex]);
+	}
+}
