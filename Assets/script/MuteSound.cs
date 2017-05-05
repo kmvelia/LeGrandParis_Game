20a90e9a1e9b20961724269b7251a151a4b53fdc
VeliaKevinMandresy <kmvelia@MacBook-Pro-de-Kevin.local>
2017-05-05 11:04:38 +0200

    [Add] Script for quit game + script for music

---
+++ Assets/script/MuteSound.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteSound : MonoBehaviour
+{
+
+	public Button btnSound;
+	public Sprite spriteBtnOn;
+	public Sprite spriteBtnOff;
+	public int counter = 0;
+
+	// Use this for initialization
+	void Start()
+	{
+		Button btnMute = btnSound.GetComponent<Button>();
+		btnMute.onClick.AddListener(muteSound);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void muteSound() {
+		counter++;
+		if (counter % 2 == 0)
+		{
+			btnSound.image.overrideSprite = spriteBtnOn;
+			AudioListener.pause = true;
+		}
+		else {
+			btnSound.image.overrideSprite = spriteBtnOff;
+			AudioListener.pause = false;
+		}
+	}
+}
